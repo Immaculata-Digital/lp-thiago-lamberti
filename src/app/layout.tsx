307baf6b3813f3b4aabb4e4b073d2431df34a5da
Macbook <macbook@Macbooks-MacBook-Air.local>
2026-04-11 23:11:55 -0300

    feat: add google tag and update runner tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,12 +40,11 @@ export default function RootLayout({
           src="https://www.googletagmanager.com/gtag/js?id=AW-18082855554"
           strategy="afterInteractive"
         />
-        <Script id="google-ads-tag" strategy="afterInteractive">
+        <Script id="google-tag" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'AW-18082855554');
           `}
         </Script>
